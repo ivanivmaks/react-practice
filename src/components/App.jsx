5d85982616155c0c2dbd11bff52b3a4145e8dff9
ivaniv_maks <ivanivmaks0@gmail.com>
2024-04-25 17:40:12 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,6 @@ import BookList from "./Mail.jsx";
 const favouriteBooks = [
   { id: "id-1", name: "JS for beginners" },
   { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" },
 ];
 
 export default function App() {
